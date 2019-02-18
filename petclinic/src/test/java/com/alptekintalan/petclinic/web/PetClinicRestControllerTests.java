af3045425c01bc6612b5fba2a50d6718ff462a7e
proje08 <proje08@PROJE-08.abas.local>
2019-02-18 22:51:23 +0300

    20190218

---
+++ petclinic/src/test/java/com/alptekintalan/petclinic/web/PetClinicRestControllerTests.java
@@ -0,0 +1,73 @@
+package com.alptekintalan.petclinic.web;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.alptekintalan.petclinic.model.Owner;
+
+public class PetClinicRestControllerTests {
+
+	private RestTemplate restTemplate;
+	
+	@Before
+	public void setUp() {
+		restTemplate = new RestTemplate();
+	}
+	
+	@Test
+	public void testGetOwnerById() {
+		ResponseEntity<Owner> response = restTemplate.getForEntity("http://localhost:8080/rest/owner/1", Owner.class);
+		MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+		MatcherAssert.assertThat(response.getBody().getFirstName(), Matchers.equalTo("Ali"));
+	}
+	
+	@Test
+	public void testGetOwnersByLastName() {
+		ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8080/rest/owner?ln=Sevindik",
+				List.class);
+
+		MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+		List<Map<String, String>> body = response.getBody();
+
+		List<String> firstNames = body.stream().map(e -> e.get("firstName")).collect(Collectors.toList());
+
+		MatcherAssert.assertThat(firstNames, Matchers.containsInAnyOrder("Kenan", "Hümeyra", "Salim"));
+	}
+	
+	@Test
+	public void testGetOwners() {
+		ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8080/rest/owners", List.class);
+		List<Map<String,String>> body = response.getBody();
+
+		MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+		
+		List<String> firstNames = body.stream().map(e->e.get("firstName")).collect(Collectors.toList());
+		
+		MatcherAssert.assertThat(firstNames, Matchers.containsInAnyOrder("Kenan", "Hümeyra", "Salim", "Muhammed"));
+	}
+	
+	@Test
+	public void testCreateOwner() {
+		Owner owner = new Owner();
+		owner.setFirstName("Metehan");
+		owner.setLastName("Yücel");
+
+		RestTemplate restTemplate = new RestTemplate();
+		URI location = restTemplate.postForLocation("http://localhost:8080/rest/owner", owner);
+
+		Owner owner2 = restTemplate.getForObject(location, Owner.class);
+
+		MatcherAssert.assertThat(owner2.getFirstName(), Matchers.equalTo(owner.getFirstName()));
+		MatcherAssert.assertThat(owner2.getLastName(), Matchers.equalTo(owner.getLastName()));
+	}
+
+}
